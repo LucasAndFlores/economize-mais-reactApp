7c153e92f8fa7d82f312b8cb5898cf468061267c
Eli Melo <eli melo>
2021-10-02 19:47:22 -0300

    primeira versao da branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from 'react-router-dom';
+
 import InitialPage from './components/noLogged/home/InitialPage';
+import TransacoesPage from './components/Logged/transacoes/TransacoesPage';
 
 function App() {
-  return (
-    <InitialPage> </InitialPage>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={InitialPage} />
+                <Route path="/transacoes" component={TransacoesPage} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
+
---
+++ src/components/logged/transacoes/TransacoesPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BackgroundContainerStyle } from '../../../shared/styles/styleBackground';
+
+
+
+
+const TransacoesPage = () => {
+    return (
+        <BackgroundContainerStyle>
+           transacoes
+        </BackgroundContainerStyle>
+    );
+}
+
+export default TransacoesPage;

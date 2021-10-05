3a4f143889057d79eb4f6124338610ff1c445832
Eli Melo <eli melo>
2021-10-04 21:22:03 -0300

    colocando pagina de transacoes

--- src/components/logged/transacoes/TransacoesPageStyle.js
+++ src/components/logged/transacoes/TransacoesPageStyle.js
@@ -1,54 +1,63 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.div `
+export const MasterContainer = styled.div`
     display: flex;
     flex-direction: row;
+    background-color:  #EFEFEF;
+    width: 100vw;
 `
-
-export const LogoStyle = styled.img`
-    width: 20%;
-    margin-right: 5%;
-`
-
-
-
-export const TitleCenter = styled.h1`
-    text-align: center;
-    font-size: 1.5em;
-`
-
-export const MainImageCenter = styled.img`
-    width: 100%;
-    height: 60vh;
-    margin-top: 20px;
-`
-
-export const MainContainer = styled.div`
-    align-items: center;
-`
-
-export const RowContainer = styled.div `
-    height: 45vh;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-    margin-top: 5vh;
-
-`
-
-export const RowItem = styled.div `
-    width: 15%;
-    height: 30vh;
+export const ColumnTwo = styled.div`
     display: flex;
     flex-direction: column;
+    width: 80%;
+    height: 100%;
+    margin-top: 35px; 
     align-items: center;
-    font-size: 1.2em; 
+`
+export const MarginResumo = styled.div`
+    margin-bottom: 40px;
     text-align: center;
-    
+`
+export const RegistroDespesas = styled.div`
+    background-color: white;
+    width: 80%;
+    height: 15vh;
+    margin: 10px 0;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`
+export const EstiloInput = styled.div`
+    margin: 0 10px;
+    display: flex;
+    line-height: 2;
+    flex-direction: column;
+    align-items: center;
+    font-weight: 700;
+
 `
 
-export const ImgSize = styled.img `
-    width: 100px;
-    height: 100px;
+export const TransactionHistory = styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    border-radius: 20px;
+    margin: 15px;
+    margin-top: 0px;
+    padding: 20px 24px;
+    height: 430px;
+
 `
+
+export const LabelFormTransacoes = styled.label`
+`
+export const InputFormTransacoes = styled.input`
+`
+export const Button = styled.button`
+`
+export const Select = styled.select`
+`
+

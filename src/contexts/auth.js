f9f2ef397d2d1ea2d08bdec79f93cf29040f0814
lucas andrade <lcs.oliveira93@gmail.com>
2021-10-11 16:15:14 -0300

    adding auth context

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,36 +1,35 @@
-import { createContext, useState, useEffect, useContext, Children } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
-import { api } from '../services/Api'
+import  { api }  from '../services/api'
 
-const AuthContext = createContext({})
+const AuthContext = createContext()
 
-export const AuthProvider = ({ childen }) => {
+function AuthProvider ({ children }) {
 
-    const [user,setUser] = useState(null)
-    const [loading, setLoading] = useState(true);
-
-    const storagedUserName = localStorage.getItem('user_name')
-    const storagedToken = localStorage.getItem('token')
-    const storagedUserId = localStorage.getItem('user_id')
+    const [ authenticated, setAuthenticated ] = useState(false)
+    const [loading, setLoading] = useState(true)
 
 
     useEffect(() => {
-
-        if (storagedToken && storagedUserId && storagedUserName) {
-            api.defaults.headers.Authorization = `Bearer ${storagedToken}`;
+        const HasToken = localStorage.getItem("token")
+        if(HasToken) {
+        api.defaults.headers.Authorization = `Bearer : ${HasToken}`  
+        setAuthenticated(true)
         }
+        setLoading(false)
+    }, [])
 
-        setTimeout(() => setLoading(false), 1000);
-    },[])
+
+
     
-    const fkUsuarioId = () => {
-        return storagedUserId
-    }
+
 
     return (
-        <AuthContext.Provider value={{ loading, fkUsuarioId }}>
-            { Children }
+        <AuthContext.Provider value={ { authenticated, setAuthenticated, loading }}>
+            { children }
         </AuthContext.Provider>
     )
     
 }
+
+export { AuthContext, AuthProvider }

10ef3491f4a7a1ad7849f80bb581f32c0e5a6aeb
lucas andrade <lcs.oliveira93@gmail.com>
2021-10-11 16:15:58 -0300

    refac routes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
 import React from 'react';
 
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
+   Router
 } from 'react-router-dom';
 
-import InitialPage from './components/noLogged/home/InitialPage';
-import LoginPage from './components/noLogged/login/LoginPage';
-import Registerpage from './components/noLogged/register/RegisterPage';
-import TransacoesPage from './components/logged/transacoes/TransacoesPage';
-import Homeapp from './components/logged/homeApp/HomeApp';
-import Configpage from './components/logged/configuracoes/ConfigPage';
-import Cartoespage from './components/logged/cartoes/CartoesPage';
-import Loader from './shared/components/loader'
+import { AuthProvider } from './contexts/auth';
+import Routes from './routes';
+import history from './history';
+
 
 import './font.css'
 
 function App() {
     return (
-        <Router>
-
-            <Switch>
-                <Route path="/" exact component={InitialPage} />
-                <Route path="/login" exact component={LoginPage} />
-                <Route path="/cadastro" exact component={Registerpage} />
-                <Route path="/transacoes" exact component={TransacoesPage} />
-                <Route path="/home" exact component={Homeapp} />
-                <Route path="/configuracoes" exact component={Configpage} />
-                <Route path="/cartoes" exact component={Cartoespage} />
-                <Route path="/loader" exact component={Loader} />
-            </Switch>
-
-        </Router>
+        <AuthProvider>
+            <Router history={history}>
+                <Routes />
+            </Router>
+        </AuthProvider>
     );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+
+import {
+    Route,
+    Switch,
+} from 'react-router-dom';
+
+import InitialPage from './components/noLogged/home/InitialPage'
+import LoginPage from './components/noLogged/login/LoginPage';
+import Registerpage from './components/noLogged/register/RegisterPage';
+import TransacoesPage from './components/logged/transacoes/TransacoesPage';
+import Homeapp from './components/logged/homeApp/HomeApp';
+import Configpage from './components/logged/configuracoes/ConfigPage';
+import Cartoespage from './components/logged/cartoes/CartoesPage';
+import Loader from './shared/components/loader'
+
+import { AuthContext } from './contexts/auth';
+
+function LoggedRouter ({ isPrivate, ...rest }) {
+    
+    const { loading, authenticated } = useContext(AuthContext)
+
+    if (isPrivate && !authenticated ) {
+        return <h2>Não autenticado </h2>
+    }
+
+    if (loading) {
+
+        return <Loader />
+    }
+
+    return <Route {...rest} />
+
+}
+
+export default function Routes() {
+    return (
+    <Switch>
+
+        <Route path="/" exact component={InitialPage} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/cadastro" exact component={Registerpage} />
+        <LoggedRouter isPrivate path="/home" exact component={Homeapp} />
+        <LoggedRouter isPrivate path="/transacoes" exact component={TransacoesPage} />
+        <LoggedRouter isPrivate path="/configuracoes" exact component={Configpage} />
+        <LoggedRouter isPrivate path="/cartoes" exact component={Cartoespage} />
+        <LoggedRouter isPrivate path="/loader" exact component={Loader} />
+
+    </Switch>
+    )
+}

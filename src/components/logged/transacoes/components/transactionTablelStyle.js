0ca4156f2adfba88e927beab682e25ea047bae20
lucas andrade <lcs.oliveira93@gmail.com>
2021-10-13 21:53:36 -0300

    adding transaction get

--- src/components/logged/transacoes/components/transactionTablelStyle.js
+++ src/components/logged/transacoes/components/transactionTablelStyle.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
+export const MainContainer = styled.div`
+    height: 60vh;
+    width: 100%;
+    overflow: auto;
+`
+
 export const TabelaGastos = styled.table`
     width: 100%;
-    height: 340px;
     text-align: left;
     border-collapse: collapse;
     background-color: white;
+
+    tr {
+     border-bottom: 1px solid #c2c2c2;
+     height: 7vh
+    }
 `
-
-
 

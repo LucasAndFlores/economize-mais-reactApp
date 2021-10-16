52662d0631f47c0e7b83eca52bea3c7cce2acc55
lucas andrade <lcs.oliveira93@gmail.com>
2021-10-16 19:39:37 -0300

    spent by type of card

---
+++ src/components/logged/homeApp/components/CardTypeTransaction/styles.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const TitleContainer = styled.h2`
+    text-align: center;
+`
+
+export const MainContainerCards = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+
+`
+export const DebitCardContainer = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+export const CreditCardContainer = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const CardAlign = styled.div`
+    width: 80%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+`
+
+export const IconCard = styled.div`
+    width: 30%;
+    height: 30%;
+`
+
+export const TitleCard = styled.p`
+    margin: 0;
+    font-size: 1.2em;
+    font-weight: bold;
+
+`
+export const SpentCard = styled.p`
+    margin: 0;
+
+`
